41351b109eebc4d2339c5a7fa1d585190c10fd1d
bautilemoine <blemoine@udesa.edu.ar>
2026-03-30 17:11:50 -0300

    app

--- pi/src/App.js
+++ pi/src/App.js
@@ -1,11 +1,22 @@
-
-import './App.css';
-import './'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer'; 
 function App() {
   return (
-    
+    <BrowserRouter>
+
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/series" element={<Series />} />
+      </Routes>
+
+      <Footer />
+
+    </BrowserRouter>
   );
 }
-
-export default App;
