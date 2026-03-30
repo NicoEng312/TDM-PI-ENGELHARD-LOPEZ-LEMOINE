47d2f1085bc35151ec98b3386d63434c8405235f
LucasLopez1 <lucaslopez@udesa.edu.ar>
2026-03-30 16:18:21 -0300

    Hice el footer

---
+++ pi/src/components/footer/footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './footer.css';
+
+function Footer() {
+  return (
+    <footer className="footer-components">
+      <p className="nombres-footer">Lucas Lopez | Bautista Lemoine | Nicolas Engelhard</p>
+    </footer>
+  );
+}
+
+export default Footer;
