992a6fc8fd2455addd371dd9e7ed86e65508298d
LucasLopez1 <lucaslopez@udesa.edu.ar>
2026-04-06 15:58:57 -0300

    Formulario register con webstorage y formulario terminado

--- pi/src/App.js
+++ pi/src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
-import Footer from './components/Footer'; 
+import Footer from './components/Footer';
 function App() {
   return (
     <BrowserRouter>
 
+
       <Header />
+
 
       <Routes>
         <Route path="/" element={<Home />} />
@@ -15,7 +17,9 @@ function App() {
         <Route path="/series" element={<Series />} />
       </Routes>
 
+
       <Footer />
+
 
     </BrowserRouter>
   );
--- pi/src/components/formularioregister/formularioregister.js
+++ pi/src/components/formularioregister/formularioregister.js
@@ -1,45 +1,65 @@
-import React from "react";
+import React, { Component } from 'react';
 
-function FormularioRegister() {
-  return (
-    <section>
-      <h2 className="mensaje-register">Registro</h2>
+class FormularioRegister extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      error: ''
+    };
+  }
 
-      <div className="caja-register">
-        <div className="cuadro-register">
-          <form>
-            <div className="form-group">
-              <label htmlFor="email">Email</label>
-              <input
-                type="email"
-                className="form-control"
-                id="email"
-                placeholder="Ingresá tu email"
-              />
-            </div>
+  evitarSubmit(event) {
+    event.preventDefault();
 
-            <div className="form-group">
-              <label htmlFor="password">Contraseña</label>
-              <input
-                type="password"
-                className="form-control"
-                id="password"
-                placeholder="Ingresá tu contraseña"
-              />
-            </div>
+    let usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
+    let emailEnUso = usuarios.some(user => user.email === this.state.email);
 
-            <button type="submit" className="button-register">
-              Registrarse
-            </button>
-          </form>
+    if (emailEnUso) {
+      this.setState({ error: 'El email ya está en uso' });
+    } else if (this.state.password.length < 6) {
+      this.setState({ error: 'La contraseña debe tener al menos 6 caracteres' });
+    } else {
+      usuarios.push({ email: this.state.email, password: this.state.password });
+      localStorage.setItem('usuarios', JSON.stringify(usuarios));
+      document.cookie = 'session=true';
+      this.setState({ error: '' });
+      window.location.href = '/';
+    }
+  }
 
-          <p className="pregunta-login">
-            ¿Ya tenés cuenta? <a href="/login">Iniciar sesión</a>
-          </p>
-        </div>
-      </div>
-    </section>
-  );
+  controlarEmail(event) {
+    this.setState({ email: event.target.value });
+  }
+
+  controlarPassword(event) {
+    this.setState({ password: event.target.value });
+  }
+
+  render() {
+    return (
+      <form onSubmit={(event) => this.evitarSubmit(event)}>
+        <label>Email:</label>
+        <input
+          type="email"
+          onChange={(event) => this.controlarEmail(event)}
+          value={this.state.email}
+        />
+
+        <label>Password:</label>
+        <input
+          type="password"
+          onChange={(event) => this.controlarPassword(event)}
+          value={this.state.password}
+        />
+
+        {this.state.error !== '' && <p className="error">{this.state.error}</p>}
+
+        <button type="submit">Crear Cuenta</button>
+      </form>
+    );
+  }
 }
 
 export default FormularioRegister;
