00c6f2c498fbbb384061b502ab9e98242ffc786a
bautilemoine <blemoine@udesa.edu.ar>
2026-04-06 22:57:56 -0300

    ver descripcion

--- pi/src/components/card/card.js
+++ pi/src/components/card/card.js
@@ -1,20 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './card.css';
 
-function Card(props){
-    return(
-        <>
-            <article className={props.claseCard}>
-                <img src={props.image} className="card-img-top" alt={props.titulo}/>
-                <div className="cardBody">
-                    <h5 className="card-title">{props.titulo}</h5>
-                    <p className="card-text">{props.descripcion}</p>
-                    <a href={props.link} className="btn btn-primary">Ver más</a>
-                    <a href="/" className={props.claseFavorito}>{props.favorito}</a>
-                </div>
-            </article>
-        </>
-    )
+class Card extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mostrarDescripcion: false
+    };
+  }
+
+  mostrarOcultarDescripcion() {
+    this.setState({ mostrarDescripcion: !this.state.mostrarDescripcion });
+  }
+
+  render() {
+    return (
+      <article className="single-card-movie">
+        <img src={this.props.image} className="card-img-top" alt={this.props.titulo} />
+        <div className="cardBody">
+          <h5 className="card-title">{this.props.titulo}</h5>
+
+          <button
+            className="btn-descripcion"
+            onClick={() => this.mostrarOcultarDescripcion()}
+          >
+            {this.state.mostrarDescripcion ? 'Ocultar descripción' : 'Ver descripción'}
+          </button>
+
+          {this.state.mostrarDescripcion && (
+            <p className="card-text">{this.props.descripcion}</p>
+          )}
+
+          <a href={this.props.link} className="btn">Ver más</a>
+        </div>
+      </article>
+    );
+  }
 }
 
-export default Card;
+export default Card; 
