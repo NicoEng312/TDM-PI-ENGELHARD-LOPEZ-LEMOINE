1306a798ee3bb19191cc753490118a219f91d554
LucasLopez1 <lucaslopez@udesa.edu.ar>
2026-04-10 13:18:35 -0300

    Hice la notfound y corregi css footer

---
+++ pi/src/screens/notfound/notfound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './notfound.css';
+
+function NotFound() {
+  return (
+    <div className="notfound">
+      <h2>404</h2>
+      <p>La página que buscás no existe</p>
+    </div>
+  );
+}
+
+export default NotFound;
