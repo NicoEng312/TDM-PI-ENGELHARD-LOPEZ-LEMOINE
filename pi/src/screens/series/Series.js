39054b0ea162eed2ae28bc6998e074729879a6a0
LucasLopez1 <lucaslopez@udesa.edu.ar>
2026-04-16 17:41:06 -0300

    Realize screen series, faltan los cargar mas

--- pi/src/screens/series/Series.js
+++ pi/src/screens/series/Series.js
@@ -1,5 +1,67 @@
-function Series() {
-  return <h1>Series</h1>;
+import React, { Component } from 'react';
+import Card from '../../components/card/card';
+
+const API_KEY = 'b1594e6717d406207ae03e5606e59437';
+
+class Series extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      series: [],
+      page: 1,
+      filtro: '',
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://api.themoviedb.org/3/tv/popular?api_key=' + API_KEY + '&language=es-ES&page=' + this.state.page)
+      .then(response => response.json())
+      .then(data => this.setState({ series: data.results }))
+      .catch(error => console.log(error));
+  }
+
+  guardarFiltro(event) {
+    this.setState({ filtro: event.target.value });
+  }
+
+  render() {
+    let seriesFiltradas = this.state.series.filter(serie =>
+    serie.name.toLowerCase().includes(this.state.filtro.toLowerCase())
+    );
+
+    return (
+      <div className="series">
+        <h1>Series</h1>
+
+        <form>
+          <input
+            placeholder="Filtrar series..."
+            onChange={(event) => this.guardarFiltro(event)}
+            value={this.state.filtro}
+          />
+        </form>
+
+        <section className="row cards">
+          {this.state.series.length === 0 ? (
+            <p>Cargando...</p>
+          ) : (
+            seriesFiltradas.map(serie => (
+              <Card
+                key={serie.id}
+                id={serie.id}
+                tipo="tv"
+                image={'https://image.tmdb.org/t/p/w342' + serie.poster_path}
+                titulo={serie.name}
+                descripcion={serie.overview}
+              />
+            ))
+          )}
+        </section>
+
+        <button>Cargar más</button>
+      </div>
+    );
+  }
 }
 
 export default Series;
