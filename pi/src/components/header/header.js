5d57fbe1abc1d175aee9f950725774fafbe1a1ff
LucasLopez1 <lucaslopez@udesa.edu.ar>
2026-04-20 16:26:46 -0300

    Cambios en header

--- pi/src/components/header/header.js
+++ pi/src/components/header/header.js
@@ -6,38 +6,27 @@ import './header.css';
 const cookies = new Cookies();
 
 class Header extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      haySession: cookies.get('auth-user') !== undefined
-    };
-  }
-
-  componentDidUpdate(prevProps) {
-    if (prevProps.location !== this.props.location) {
-      this.setState({ haySession: cookies.get('auth-user') !== undefined });
-    }
-  }
-
+  
   cerrarSesion() {
     cookies.remove('auth-user');
-    this.setState({ haySession: false });
     this.props.history.push('/');
   }
 
   render() {
+    const haySession = cookies.get('auth-user') ? true : false;
+
     return (
       <header>
         <Link to="/"><h1>MovieApp</h1></Link>
         <nav>
           <ul>
             <li><Link to="/">Home</Link></li>
-            {!this.state.haySession && <li><Link to="/login">Login</Link></li>}
-            {!this.state.haySession && <li><Link to="/register">Crear Cuenta</Link></li>}
-            {this.state.haySession && <li><Link to="/favoritos">Favoritos</Link></li>}
+            {!haySession && <li><Link to="/login">Login</Link></li>}
+            {!haySession && <li><Link to="/register">Crear Cuenta</Link></li>}
+            {haySession && <li><Link to="/favoritos">Favoritos</Link></li>}
             <li><Link to="/movies">Películas</Link></li>
             <li><Link to="/series">Series</Link></li>
-            {this.state.haySession &&
+            {haySession &&
               <li><button onClick={() => this.cerrarSesion()}>Cerrar Sesión</button></li>
             }
           </ul>
