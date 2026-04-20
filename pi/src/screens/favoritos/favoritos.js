da1a9ee991ec45ce004ac8f8df2f64bef57fc05d
LucasLopez1 <lucaslopez@udesa.edu.ar>
2026-04-20 15:58:33 -0300

    Secciono favoritos entre series y peliculas

--- pi/src/screens/favoritos/favoritos.js
+++ pi/src/screens/favoritos/favoritos.js
@@ -18,25 +18,54 @@ class Favoritos extends Component {
     this.setState({ favoritos: favoritos });
   }
   render() {
+    let peliculasFavoritas = this.state.favoritos.filter(fav => fav.tipo === 'movie');
+    let seriesFavoritas = this.state.favoritos.filter(fav => fav.tipo === 'tv');
+
     return (
       <div className="home">
         <h2>Mis Favoritos</h2>
-        <section className="row cards">
-          {this.state.favoritos.length === 0 ? (
-            <p>No tenés favoritos todavía.</p>
-          ) : (
-            this.state.favoritos.map(pelicula => (
-              <Card
-                key={pelicula.id}
-                id={pelicula.id}
-                tipo={pelicula.tipo}
-                image={pelicula.image}
-                titulo={pelicula.titulo}
-                descripcion={pelicula.descripcion}
-              />
-            ))
-          )}
-        </section>
+
+        {this.state.favoritos.length === 0 ? (
+          <p>No tenés favoritos todavía.</p>
+        ) : (
+          <>
+            <h2>Películas</h2>
+            <section className="row cards">
+              {peliculasFavoritas.length === 0 ? (
+                <p>No tenés películas favoritas todavía.</p>
+              ) : (
+                peliculasFavoritas.map(pelicula => (
+                  <Card
+                    key={pelicula.id}
+                    id={pelicula.id}
+                    tipo={pelicula.tipo}
+                    image={pelicula.image}
+                    titulo={pelicula.titulo}
+                    descripcion={pelicula.descripcion}
+                  />
+                ))
+              )}
+            </section>
+
+            <h2>Series</h2>
+            <section className="row cards">
+              {seriesFavoritas.length === 0 ? (
+                <p>No tenés series favoritas todavía.</p>
+              ) : (
+                seriesFavoritas.map(serie => (
+                  <Card
+                    key={serie.id}
+                    id={serie.id}
+                    tipo={serie.tipo}
+                    image={serie.image}
+                    titulo={serie.titulo}
+                    descripcion={serie.descripcion}
+                  />
+                ))
+              )}
+            </section>
+          </>
+        )}
       </div>
     );
   }
